4d4da2d7a8c27d6540cd514ca498d3e626180ab5
S1107327 <s1107327@studenti.univpm.it>
2022-03-03 12:24:09 +0100

    Aggiornamento SMTChecker

--- contracts/CarbonFootprint.sol
+++ contracts/CarbonFootprint.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.0 <0.9.0;
+pragma experimental SMTChecker;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ProductLibrary.sol";
