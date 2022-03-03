a0bce66957d3a87729b445aae424454bc16f5dce
S1107327 <s1107327@studenti.univpm.it>
2022-03-03 17:03:33 +0100

    Aggiornamento da rivedere

--- Off-chain/connection.py
+++ Off-chain/connection.py
@@ -1,5 +1,4 @@
 from web3 import Web3
-
 baseURL = "http://127.0.0.1:2200"
 
 
@@ -7,3 +6,4 @@ def connect(role):
     URL = baseURL + str(role)
     web3 = Web3(Web3.HTTPProvider(URL))
     print(web3.isConnected())
+    return web3
--- Off-chain/main.py
+++ Off-chain/main.py
@@ -1,8 +1,31 @@
 import re
+from connection import connect
 
-userAddress = input("Benvenuto, per iniziare inserisci il tuo portafoglio elettronico: ").strip()
-while not re.match("^0x[A-Za-z0-9]{40}$", userAddress):
-    print("Il formato dell'indirizzo inserito non è corretto. Inserire un nuovo indirizzo")
-    userAddress = input("Inserisci il tuo portafoglio elettronico: ").strip()
-"""Ruolo = input("Specifica se sei un fornitore, un trasformatore o un cliente per richiedere delle operazioni: ").strip()"""
+ruolo = int(input("Specifica se sei un fornitore, un trasformatore o un cliente per richiedere delle operazioni: ").strip())
+web3 = connect(ruolo)
+print("Ecco le operazioni che puoi fare: ")
+choice = ''
+if ruolo == 0:
+    # istanziazione oggetto che gestisce gli utenti di un ruolo
+    while choice != 'E':
+        print("Ricerca di uno o più prodotti(C)")
+        print("Uscire(E)")
+        choice = input("Digita una lettera per scegliere: ")
+if ruolo == 1:
+    # istanziazione oggetto che gestisce gli utenti di un ruolo
+    while choice != 'E':
+        print("Ricerca di uno o più prodotti(C)")
+        print("Aggiunta di un nuovo prodotto(P)")
+        print("Trasferimento del possesso di un prodotto(S)")
+        print("Uscire(E)")
+        choice = input("Digita una lettera per scegliere: ")
+if ruolo == 2:
+    # istanziazione oggetto che gestisce gli utenti di un ruolo
+    while choice != 'E':
+        print("Ricerca di uno o più prodotti(C)")
+        print("Inserimento di una nuova trasformazione(T)")
+        print("Trasferimento del possesso di un prodotto(S)")
+        print("Uscire(E)")
+        choice = input("Digita una lettera per scegliere: ")
+print("Arrivederci")
 
