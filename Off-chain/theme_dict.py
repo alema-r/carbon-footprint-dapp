bfd693defdde013e9be989b78f97246b63f23c64
lorenzo <s1107327@studenti.univpm.it>
2022-04-10 11:09:18 +0200

    temi

---
+++ Off-chain/theme_dict.py
@@ -0,0 +1,19 @@
+theme = {
+  "Question": {
+    "mark_color": "yellow",
+    "brackets_color": "bright_green"
+  },
+  "List": {
+    "selection_color": "white_on_bright_red",
+    "selection_cursor": "↳",
+    "unselected_color": "green"
+  },
+  "Checkbox": {
+    "selection_color": "white_on_bright_red",
+    "selection_icon": "⤷",
+    "selected_icon": "☑",
+    "selected_color": "green",
+    "unselected_color": "red",
+    "unselected_icon": "☐"
+  }
+}
