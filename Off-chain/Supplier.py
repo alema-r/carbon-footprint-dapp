c7879b686272baf6b9701cfaadbb0037f43888d0
lorenzo <s1107327@studenti.univpm.it>
2022-04-10 18:26:08 +0200

    aggiornamento grafica e Ctrl+C per annullare

--- Off-chain/Supplier.py
+++ Off-chain/Supplier.py
@@ -1,4 +1,6 @@
 import inquirer
+from inquirer.themes import load_theme_from_dict
+from theme_dict import theme
 from BlockChain import create_raw_materials_on_blockchain
 import validation
 from Models import RawMaterial
@@ -33,61 +35,65 @@ def insert_raw_material():
     """
     raw_materials = []
     action = "start"
-
     # This while is used to manage the interaction with the supplier
-    while (action != "Register raw materials") & (action != "Cancel insertion"):
+    while (action != "Register raw materials") and (action != "Cancel insertion") and action is not None:
         # List of actions the user can perform
         if len(raw_materials) > 0:
-            action = inquirer.list_input(
+            question = [inquirer.List(
+                'action',
                 message="Choose which action you want to perform",
                 choices=["Add raw material", "Register raw materials", "Cancel insertion"]
-            )
+            )]
         else:
-            action = inquirer.list_input(
+            question = [inquirer.List(
+                'action',
                 message="Choose which action you want to perform",
                 choices=["Add raw material", "Cancel insertion"]
-            )
+            )]
+        action = inquirer.prompt(question, theme=load_theme_from_dict(theme))
         # List of input the user should insert if he chooses to add new raw material
-        if action == "Add raw material":
-            questions = [
-                inquirer.Text('raw material',
-                              message="Insert new raw material name",
-                              validate=validation.name_input_validation
-                              ),
-                inquirer.Text('lot',
-                              message="Insert raw material's lot",
-                              validate=validation.lot_input_validation
-                              ),
-                inquirer.Text('carbon footprint',
-                              message="Insert raw material carbon footprint",
-                              validate=validation.carbon_fp_input_validation
-                              )
-            ]
-            # This line show all the questions coded above and the put the user's answers inside "answers" variable
-            answers = inquirer.prompt(questions)
-            # New raw material instance generated using user's inputs values
-            raw_material_to_check = RawMaterial(answers["raw material"], int(answers['lot']), web3.eth.default_account,
-                                                int(answers['carbon footprint']))
-            # The new raw material is validated. 
-            valid, error_message = input_validation(raw_material_to_check, raw_materials)
-            # If the new raw material is valid it is appended in the raw materials list 
-            if valid:
-                raw_materials.append(raw_material_to_check)
-                print("New raw material correctly inserted")
-            # If the added raw material is not valid an error message is shown to the user
+        if action is not None:
+            if action['action'] == "Add raw material":
+                questions = [
+                    inquirer.Text('raw material',
+                                  message="Insert new raw material name",
+                                  validate=validation.name_input_validation
+                                  ),
+                    inquirer.Text('lot',
+                                  message="Insert raw material's lot",
+                                  validate=validation.lot_input_validation
+                                  ),
+                    inquirer.Text('carbon footprint',
+                                  message="Insert raw material carbon footprint",
+                                  validate=validation.carbon_fp_input_validation
+                                  )
+                ]
+                # This line show all the questions coded above and the put the user's answers inside "answers" variable
+                answers = inquirer.prompt(questions, theme=load_theme_from_dict(theme))
+                # New raw material instance generated using user's inputs values
+                if answers is not None:
+                    raw_material_to_check = RawMaterial(answers["raw material"], int(answers['lot']), web3.eth.default_account,
+                                                        int(answers['carbon footprint']))
+                    # The new raw material is validated.
+                    valid, error_message = input_validation(raw_material_to_check, raw_materials)
+                    # If the new raw material is valid it is appended in the raw materials list
+                    if valid:
+                        raw_materials.append(raw_material_to_check)
+                        print("New raw material correctly inserted")
+                    # If the added raw material is not valid an error message is shown to the user
+                    else:
+                        print(f"Invalid input: {error_message}")
+
+            # If the user chooses to Cancel the operation all inserted inputs are destroyed and the functions ends
+            elif action['action'] == "Cancel insertion":
+                raw_materials = []
+                return
+            # When the user select Done, the interactions ends and the new added raw materials are insertend inside the blockchain
             else:
-                print(f"Invalid input: {error_message}")
-
-        # If the user chooses to Cancel the operation all inserted inputs are destroyed and the functions ends
-        elif action == "Cancel":
-            raw_materials = []
-            return
-
-        # When the user select Done, the interactions ends and the new added raw materials are insertend inside the blockchain
-        else:
-            try:
-                create_raw_materials_on_blockchain(raw_materials)
-            # If the inserting operation fails, an error is printed.
-            except Exception as e:
-                print(e)
-                print("Please insert raw materials again")
+                try:
+                    if len(raw_materials) > 0:
+                        create_raw_materials_on_blockchain(raw_materials)
+                # If the inserting operation fails, an error is printed.
+                except Exception as e:
+                    print(e)
+                    print("Please insert raw materials again")
