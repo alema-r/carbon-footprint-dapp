cfe3ce3dae17aa0779a3046c420eacfb3f8a87d9
alema-r <alessandromarcolini99@gmail.com>
2022-04-11 11:36:37 +0200

    Add option to only compile sol files without smtc

--- compile.py
+++ compile.py
@@ -1,24 +1,54 @@
+"""
+Script used to compile solidity contracts.
+"""
+
 import json
 import shutil
 import subprocess
+import inquirer
 
-solc_bin = shutil.which("solc")
-if solc_bin == "":
-    print("Eseguibile solc non trovato.")
-    exit(1)
-solc_options = "solc_options.json"
-cmd = f"{solc_bin} --base-path . --include-path .deps/npm/ --standard-json {solc_options} --pretty-json"
-output = subprocess.check_output(cmd.split(" "))
-output = json.loads(output)
+NODE_MOD_PATH = "node_modules/"
 
-with open("solc_output/errors.json", "w") as errors:
-    json.dump(obj=output["errors"], fp=errors)
 
-with open("solc_output/CFContract.json", "w") as cf_contract:
-    json.dump(
-        obj=output["contracts"]["contracts/CarbonFootprint.sol"]["CarbonFootprint"],
-        fp=cf_contract,
-    )
+def main():
+    choices = [
+        "Compile with SMTChecker (don't choose this option if you are using Windows)",
+        "Only compile",
+    ]
 
-with open("solc_output/UserContract.json", "w") as user_contract:
-    json.dump(obj=output["contracts"]["contracts/User.sol"]["User"], fp=user_contract)
+    choice = inquirer.list_input(message="Choose an option", choices=choices)
+    solc_bin = shutil.which("solc")
+    if solc_bin is None:
+        print("Cannot find solidity executable.")
+        exit(1)
+
+    print("Compiling...")
+    if choice == choices[0]:
+        solc_options = "solc_options_smt.json"
+        cmd = f"{solc_bin} --base-path . --include-path {NODE_MOD_PATH} --standard-json {solc_options} --pretty-json"
+        output = subprocess.check_output(cmd.split(" "))
+        output = json.loads(output)
+
+        with open("solc_output/errors.json", "w", encoding="utf8") as errors:
+            json.dump(obj=output["errors"], fp=errors, indent=4)
+
+    else:
+        solc_options = "solc_options.json"
+        cmd = f"{solc_bin} --base-path . --include-path {NODE_MOD_PATH} --standard-json {solc_options} --pretty-json"
+        output = subprocess.check_output(cmd.split(" "))
+        output = json.loads(output)
+
+    print("Done!")
+    with open("solc_output/CFContract.json", "w", encoding="utf8") as cf_contract:
+        json.dump(
+            obj=output["contracts"]["contracts/CarbonFootprint.sol"]["CarbonFootprint"],
+            fp=cf_contract,
+        )
+    with open("solc_output/UserContract.json", "w", encoding="utf8") as user_contract:
+        json.dump(
+            obj=output["contracts"]["contracts/User.sol"]["User"], fp=user_contract
+        )
+
+
+if __name__ == "__main__":
+    main()
